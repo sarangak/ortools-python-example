73b1dd36f72678ee2b3de41f46bcc9a0ec451ca5
Saranga Komanduri <sarangak@cs.cmu.edu>
2020-04-20 21:43:40 -0700

    Use built-in printer

--- csp_python.py
+++ csp_python.py
@@ -29,23 +29,6 @@ n. The Norwegian lives next to the blue house.
 from ortools.sat.python import cp_model
 
 
-class VarArraySolutionPrinter(cp_model.CpSolverSolutionCallback):
-    """Print intermediate solutions."""
-    def __init__(self, variables):
-        cp_model.CpSolverSolutionCallback.__init__(self)
-        self.__variables = variables
-        self.__solution_count = 0
-
-    def on_solution_callback(self):
-        self.__solution_count += 1
-        for v in self.__variables:
-            print('%s=%i' % (v, self.Value(v)), end=' ')
-        print()
-
-    def solution_count(self):
-        return self.__solution_count
-
-
 def CPIsFun():
     # Constraint programming engine
     model = cp_model.CpModel()
@@ -154,7 +137,7 @@ def CPIsFun():
 
     # Solve model.
     solver = cp_model.CpSolver()
-    solution_printer = VarArraySolutionPrinter([
+    solution_printer = cp_model.VarArraySolutionPrinter([
         item
         for sublist in [colorcons, nationcons, drinkcons, petcons, smokecons]
         for item in sublist
